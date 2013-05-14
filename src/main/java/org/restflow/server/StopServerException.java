9bd89982d31bb37b0f59f37d94b5526263329327
Timothy McPhillips <tmcphillips@absoluteflow.org>
2013-05-13 21:42:20 -0700

    Moved RestFlow server code to RestFlowServer project.

--- src/main/java/org/restflow/server/StopServerException.java
+++
@@ -1,8 +0,0 @@
-package org.restflow.server;
-
-public class StopServerException extends Exception {
-	public StopServerException(String m) {
-		super(m);
-	}
-	private static final long serialVersionUID = 4733287016804683315L;
-}
