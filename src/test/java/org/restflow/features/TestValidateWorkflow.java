8f62d39a72b15bae891af81984ec144de7026f66
Timothy McPhillips <tmcphillips@absoluteflow.org>
2013-05-27 21:47:39 -0700

    Test now checks for expected stderr output.

--- src/test/java/org/restflow/features/TestValidateWorkflow.java
+++ src/test/java/org/restflow/features/TestValidateWorkflow.java
@@ -2,38 +2,29 @@ package org.restflow.features;
 
 import org.restflow.RestFlow;
 import org.restflow.WorkflowRunner;
-import org.restflow.metadata.RunMetadata;
 import org.restflow.test.RestFlowTestCase;
+import org.restflow.util.StdoutRecorder;
 
 
-public class TestValidateOnly extends RestFlowTestCase {
+public class TestValidateWorkflow extends RestFlowTestCase {
 
 	public void setUp() throws Exception {
 		super.setUp();
 	}
 	
-	public void testOne() throws Exception {
+	public void testValidate_NoOutflowVariablesInTemplate() throws Exception {
 		
-		String workflowFile = "src/test/resources/ssrl/workflow/RestFlow/workflow_fileProtocolWithoutUriVariables" +
+		final String workflowFile = "classpath:/org/restflow/TestValidateWorkflow/FileProtocolWithoutOutflowUriVariables" +
 			WorkflowRunner.YAML_EXTENSION;
 
-		@SuppressWarnings("unused")
-		RunMetadata metadata = RestFlow.loadAndRunWorkflow( 
-			new String[]{ "-v", "-f", workflowFile, "-base", "RESTFLOW_TESTRUNS_DIR" } );
+		StdoutRecorder recorder = new StdoutRecorder(new StdoutRecorder.WrappedCode() {
+			public void execute() throws Exception {
+				RestFlow.loadAndRunWorkflow( 
+					new String[]{ "-v", "-f", workflowFile, "-base", "RESTFLOW_TESTRUNS_DIR" } );
+			}
+		});
+		
+		assertTrue(recorder.getStderrRecording().contains(
+				"No variables in outflow template with file scheme: file:/messages/greeting on CreateGreeting"));
 	}
-
-//	public void testAutodrug() throws Exception {
-//		
-//		String workflowFile = "/Users/tmcphillips/AutoDrug/EclipseWorkspace/Workflows/src/main/workflows/Green" +
-//			WorkflowRunner.YAML_EXTENSION;
-//
-//		RunMetaData metadata = RestFlow.loadAndRunWorkflow( 
-//			new String[]{ 
-//				"-v", 
-//				"-f", 		workflowFile,
-//				"-w", 		"AutoDrugWorkflow",
-//				"-base", 	"RESTFLOW_TESTRUNS_DIR" 
-//				} );
-//	}
-
 }
